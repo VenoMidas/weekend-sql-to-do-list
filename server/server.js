0f71c35c9d05c178be3d06e6ea8c78b8be77f859
Mark Schumacher <schuma1022@gmail.com>
2022-08-04 11:24:50 -0500

    Spin up server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.use(express.static('server/public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('Listening on port:', port);
+});

b652432d9dca465e370cf997469529fd622e0d85
Mark Schumacher <schuma1022@gmail.com>
2022-08-07 11:54:10 -0500

    Add get and post routes for tasks

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,68 @@ $(document).ready(readyNow);
 
 function readyNow() {
     // console.log('Hello jQuery');
+    getTasks()
+    submitButtonHandler();
+};
+
+function submitButtonHandler() {
+    $('#submit-task').on('click', () => {
+        if ($('#task').val() === '') {
+            return;
+        } else {
+            // console.log('in SubmitButtonHandler');
+            let taskToSend = {
+                task: $('#task').val(),
+                details: $('#details').val(),
+                priority: $('#priority').val()
+            };
+            // console.log(taskToSend);
+            saveTask(taskToSend);
+        };
+    });
+};
+
+function saveTask(newTask) {
+    // console.log(newTask);
+    $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: newTask
+    }).catch(function (error) {
+        console.log(error);
+        alert('Something went wrong!');
+    });
+};
+
+function getTasks() {
+    // console.log('In getTasks');
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+    }).then(function (response) {
+        console.log(response);
+        $('#to-do-list').empty();
+        for(let task of response) {
+            $('#to-do-list').append(`
+            <div class="list-group-item py-3">
+            <span class="h5">
+                <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
+                ${task.task}
+            </span>
+            <p class="my-2 ps-5">${task.details}</p>
+            <ul class="list-inline m-0 text-end">
+                <li class="list-inline-item">
+                    <button class="btn btn-warning btn-sm rounded-0" type="button" title="edit"><i class="bi bi-pencil-square"></i>
+                </li>
+                <li class="list-inline-item">
+                    <button class="btn btn-danger btn-sm rounded-0" type="button" title="delete"><i class="bi bi-trash3"></i>
+                </li>
+            </ul>
+        </div>
+            `);
+        }
+    }).catch(function (error) {
+        console.log(error);
+        alert('Something went wrong!');
+    });
 };
--- server/server.js
+++ server/server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+const tasks = [];
 
 app.use(express.static('server/public'));
 app.use(express.urlencoded({ extended: true }));
 
+app.post('/tasks', (req, res) => {
+    const task = req.body;
+    tasks.push(task);
+    // console.log(tasks);
+    res.sendStatus(201);
+});
 
-
-
-
+app.get('/tasks', (req, res) => {
+    res.send(tasks);
+});
 
 app.listen(port, () => {
     console.log('Listening on port:', port);
